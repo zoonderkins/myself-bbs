8803ac136294d40bef162ff2462e8de2d6e8dc09
Edward Oo <edwardoo@edwardoo-kkcompany-8.local>
2023-09-28 10:52:58 +0800

    fix: bump version

--- src/backend/constants.ts
+++ src/backend/constants.ts
@@ -1,2 +1,3 @@
 export const VERBOSE = ["1", "true", "on", "yes"].includes(process.env.VERBOSE);
-export const REMOTE_CACHE = process.env.REMOTE_CACHE || "https://myself-bbs.jacob.workers.dev";
+export const REMOTE_CACHE = process.env.REMOTE_CACHE || "https://myself-bbs-api.okz.workers.dev";
+// export const REMOTE_CACHE = process.env.REMOTE_CACHE || "https://myself-bbs.jacob.workers.dev";
